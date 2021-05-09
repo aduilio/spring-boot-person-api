ce8f6b406ca5b9a94c36cacad831e0abd1d9442f
Aduilio Silva <developer.aduilio@gmail.com>
2021-05-09 11:07:52 -0300

    Unit test PersonService

---
+++ src/test/java/com/aduilio/personapi/service/PersonServiceTest.java
@@ -0,0 +1,109 @@
+package com.aduilio.personapi.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.aduilio.personapi.dto.PersonDTO;
+import com.aduilio.personapi.entity.Person;
+import com.aduilio.personapi.exception.PersonNotFoundException;
+import com.aduilio.personapi.repository.PersonRepository;
+import com.aduilio.personapi.utils.PersonUtils;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+	@Mock
+	private PersonRepository personRepositoryMock;
+
+	@InjectMocks
+	private PersonService personService;
+
+	@Test
+	void createWithValuesShouldReturnId() {
+		final PersonDTO personDto = PersonUtils.createPersonDTO();
+		final Person person = PersonUtils.createPerson();
+
+		when(personRepositoryMock.save(person)).thenReturn(person);
+
+		final Long result = personService.create(personDto);
+
+		assertThat(result).isEqualTo(person.getId());
+	}
+
+	@Test
+	void readWithValidIdShouldReturnPerson() throws PersonNotFoundException {
+		final PersonDTO personDto = PersonUtils.createPersonDTO();
+		final Person person = PersonUtils.createPerson();
+
+		when(personRepositoryMock.findById(person.getId())).thenReturn(Optional.of(person));
+
+		final PersonDTO result = personService.read(PersonUtils.ID);
+
+		assertThat(result).isEqualTo(personDto);
+	}
+
+	@Test
+	void readWithInvalidIdShouldThrowException() throws PersonNotFoundException {
+		final Person person = PersonUtils.createPerson();
+
+		when(personRepositoryMock.findById(person.getId())).thenReturn(Optional.empty());
+
+		assertThrows(PersonNotFoundException.class, () -> personService.read(PersonUtils.ID));
+	}
+
+	@Test
+	void delateWithValidIdShouldExecute() throws PersonNotFoundException {
+		final Person person = PersonUtils.createPerson();
+
+		when(personRepositoryMock.findById(person.getId())).thenReturn(Optional.of(person));
+
+		personService.delete(PersonUtils.ID);
+
+		verify(personRepositoryMock, times(1)).findById(person.getId());
+	}
+
+	@Test
+	void deleteWithInvalidIdShouldThrowException() throws PersonNotFoundException {
+		final Person person = PersonUtils.createPerson();
+
+		when(personRepositoryMock.findById(person.getId())).thenReturn(Optional.empty());
+
+		assertThrows(PersonNotFoundException.class, () -> personService.delete(PersonUtils.ID));
+	}
+
+	@Test
+	void updateWithValidIdShouldReturnPerson() throws PersonNotFoundException {
+		final PersonDTO personDto = PersonUtils.createPersonDTO();
+		final PersonDTO personDtoUpdate = PersonUtils.createPersonDTOUpdate();
+		final Person person = PersonUtils.createPerson();
+		final Person personUpdated = PersonUtils.createPersonUpdated();
+
+		when(personRepositoryMock.findById(person.getId())).thenReturn(Optional.of(person));
+		when(personRepositoryMock.save(person)).thenReturn(personUpdated);
+
+		final PersonDTO result = personService.update(personDto.getId(), personDto);
+
+		assertThat(result).isEqualTo(personDtoUpdate);
+		verify(personRepositoryMock, times(1)).findById(person.getId());
+	}
+
+	@Test
+	void updateWithInvalidIdShouldThrowException() throws PersonNotFoundException {
+		final PersonDTO personDto = PersonUtils.createPersonDTO();
+
+		when(personRepositoryMock.findById(personDto.getId())).thenReturn(Optional.empty());
+
+		assertThrows(PersonNotFoundException.class, () -> personService.update(personDto.getId(), personDto));
+	}
+}
---
+++ src/test/java/com/aduilio/personapi/utils/PersonUtils.java
@@ -0,0 +1,81 @@
+package com.aduilio.personapi.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+
+import com.aduilio.personapi.dto.PersonDTO;
+import com.aduilio.personapi.entity.Person;
+import com.aduilio.personapi.entity.Phone;
+import com.aduilio.personapi.enums.PhoneType;
+
+public class PersonUtils {
+
+	public static final Long ID = 1L;
+	public static final String FIRST_NAME = "fName";
+	public static final String LAST_NAME = "lName";
+	public static final String BIRTH_DATE_STR = "01-01-2000";
+	public static final String CPF = "123-456-789-00";
+	public static final LocalDate BIRTH_DATE_LDATE = LocalDate.parse(BIRTH_DATE_STR,
+			DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+	public static final String PHONE_NUMBER = "12 3456-7890";
+	public static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+
+	public static Person createPerson() {
+		return Person.builder()
+				.id(ID)
+				.firstName(FIRST_NAME)
+				.lastName(LAST_NAME)
+				.birthDate(BIRTH_DATE_LDATE)
+				.cpf(CPF)
+				.phones(Collections.singletonList(Phone.builder()
+						.number(PHONE_NUMBER)
+						.type(PHONE_TYPE)
+						.build()))
+				.build();
+	}
+
+	public static Person createPersonUpdated() {
+		return Person.builder()
+				.id(ID)
+				.firstName("New" + FIRST_NAME)
+				.lastName("New" + LAST_NAME)
+				.birthDate(BIRTH_DATE_LDATE)
+				.cpf(CPF)
+				.phones(Collections.singletonList(Phone.builder()
+						.number(PHONE_NUMBER)
+						.type(PHONE_TYPE)
+						.build()))
+				.build();
+	}
+
+	public static PersonDTO createPersonDTO() {
+		return PersonDTO.builder()
+				.id(ID)
+				.firstName(FIRST_NAME)
+				.lastName(LAST_NAME)
+				.birthDate(BIRTH_DATE_STR)
+				.cpf(CPF)
+				.phones(Collections.singletonList(Phone.builder()
+						.number(PHONE_NUMBER)
+						.type(PHONE_TYPE)
+						.build()))
+				.build();
+	}
+
+	public static PersonDTO createPersonDTOUpdate() {
+		return PersonDTO.builder()
+				.id(ID)
+				.firstName("New" + FIRST_NAME)
+				.lastName("New" + LAST_NAME)
+				.birthDate(BIRTH_DATE_STR)
+				.cpf(CPF)
+				.phones(Collections.singletonList(Phone.builder()
+						.number(PHONE_NUMBER)
+						.type(PHONE_TYPE)
+						.build()))
+				.build();
+	}
+
+}
